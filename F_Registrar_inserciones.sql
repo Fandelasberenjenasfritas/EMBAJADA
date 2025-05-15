cdeda4db1bed089be2c01a1d7b855b102eed6f40
Chemamu <josemariamartinez2303@gmail.com>
2025-05-15 09:04:37 +0200

    Update F_Registrar_inserciones.sql

--- F_Registrar_inserciones.sql
+++ F_Registrar_inserciones.sql
@@ -1,12 +1,9 @@
--- Tabla que regsitra el día y la hora de cuando se inserta algo en una tabla 
-
--- Tabla que asocia a cada tipo de procedimiento un id
-CREATE VIEW IF NOT EXISTS tipos_tabla (
+CREATE TABLE IF NOT EXISTS tipos_tabla (
     idtabla TINYINT PRIMARY KEY AUTO_INCREMENT,
     table_name VARCHAR(50) NOT NULL UNIQUE
 );
 
--- Ignoramos ciudadanos y usuarios que no nos interesan
+
 INSERT IGNORE INTO tipos_tabla (table_name) VALUES   -- estoy asociando un idtabla (un numero que se incrementa solo), a las tablas que me interesa asociar a un número
 ('certificadoMatrimonio'),
 ('registroMatrimonio'),
@@ -25,15 +22,179 @@ INSERT IGNORE INTO tipos_tabla (table_name) VALUES   -- estoy asociando un idtab
 ('datosHacerUnPoder'),
 ('certificadoContinuidadMatrimonio');
 
-
 CREATE TABLE IF NOT EXISTS registros (
     idregistro TINYINT PRIMARY KEY AUTO_INCREMENT, -- numero de procedimiento registrado ese dia
     numtabla TINYINT NOT NULL ,                    -- el num que va asociado a la tabla según la tabla anterior
     fechahora DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,   -- fechahora del sistema
-    FOREIGN KEY (numtabla) REFERENCES tipos_tabla(tipos_tabla.idtabla)  
+    FOREIGN KEY (numtabla) REFERENCES tipos_tabla(idtabla)  
 );
 
--- funcion que me da el tipo de tabla 
+
+DELIMITER //
+CREATE TRIGGER certificadoMatrimonio_id
+AFTER INSERT ON certificadoMatrimonio
+FOR EACH ROW
+BEGIN
+    INSERT INTO registros (numtabla)
+    VALUES (FIDtabla('certificadoMatrimonio'));
+END//
+DELIMITER;
+
+DELIMITER //
+
+CREATE TRIGGER apostilla_id
+AFTER INSERT ON apostilla
+FOR EACH ROW
+BEGIN
+    INSERT INTO registros (numtabla)
+    VALUES (FIDtabla('apostilla'));
+END//
+DELIMITER;
+
+DELIMITER //
+
+CREATE TRIGGER certificadocontinuidadmatrimonio_id
+AFTER INSERT ON certificadocontinuidadmatrimonio
+FOR EACH ROW
+BEGIN
+    INSERT INTO registros (numtabla)
+    VALUES (FIDtabla('certificadocontinuidadmatrimonio'));
+END//
+DELIMITER;
+
+DELIMITER //
+
+CREATE TRIGGER certificadoinscripcionconsular_id
+AFTER INSERT ON certificadoinscripcionconsular
+FOR EACH ROW
+BEGIN
+    INSERT INTO registros (numtabla)
+    VALUES (FIDtabla('certificadoinscripcionconsular')); -- llamo a la función para la única variable de la tabla registros que no se rellena sola 
+END//
+DELIMITER;
+
+DELIMITER //
+
+CREATE TRIGGER certificadonacionalidad_id
+AFTER INSERT ON certificadonacionalidad
+FOR EACH ROW
+BEGIN
+    INSERT INTO registros (numtabla)
+    VALUES (FIDtabla('certificadonacionalidad'));
+END//
+DELIMITER;
+
+DELIMITER //
+
+CREATE TRIGGER datoshacerunpoder_id
+AFTER INSERT ON datoshacerunpoder
+FOR EACH ROW
+BEGIN
+    INSERT INTO registros (numtabla)
+    VALUES (FIDtabla('datoshacerunpoder'));
+END//
+DELIMITER;
+
+DELIMITER //
+
+CREATE TRIGGER declaracionesjuradas_id
+AFTER INSERT ON declaracionesjuradas
+FOR EACH ROW
+BEGIN
+    INSERT INTO registros (numtabla)
+    VALUES (FIDtabla('declaracionesjuradas'));
+END//
+DELIMITER;
+
+DELIMITER //
+
+CREATE TRIGGER documentoviaje_id
+AFTER INSERT ON documentoviaje
+FOR EACH ROW
+BEGIN
+    INSERT INTO registros (numtabla)
+    VALUES (FIDtabla('documentoviaje'));
+END//
+DELIMITER;
+
+DELIMITER //
+
+CREATE TRIGGER fedevida_id
+AFTER INSERT ON fedevida
+FOR EACH ROW
+BEGIN
+    INSERT INTO registros (numtabla)
+    VALUES (FIDtabla('fedevida'));
+END//
+DELIMITER;
+
+DELIMITER //
+
+CREATE TRIGGER legalizaciones_id
+AFTER INSERT ON legalizaciones
+FOR EACH ROW
+BEGIN
+    INSERT INTO registros (numtabla)
+    VALUES (FIDtabla('legalizaciones'));
+END//
+DELIMITER;
+
+DELIMITER //
+
+CREATE TRIGGER reconocimientoslegales_id
+AFTER INSERT ON reconocimientoslegales
+FOR EACH ROW
+BEGIN
+    INSERT INTO registros (numtabla)
+    VALUES (FIDtabla('reconocimientoslegales'));
+END//
+DELIMITER;
+
+DELIMITER //
+
+CREATE TRIGGER registrodefuncion_id
+AFTER INSERT ON registrodefuncion
+FOR EACH ROW
+BEGIN
+    INSERT INTO registros (numtabla)
+    VALUES (FIDtabla('registrodefuncion'));
+END//
+DELIMITER;
+
+DELIMITER //
+
+CREATE TRIGGER registromatrimonio_id
+AFTER INSERT ON registromatrimonio
+FOR EACH ROW
+BEGIN
+    INSERT INTO registros (numtabla)
+    VALUES (FIDtabla('registromatrimonio'));
+END//
+DELIMITER;
+
+DELIMITER //
+
+CREATE TRIGGER registronacimiento_id
+AFTER INSERT ON registronacimiento
+FOR EACH ROW
+BEGIN
+    INSERT INTO registros (numtabla)
+    VALUES (FIDtabla('registronacimiento'));
+END//
+DELIMITER;
+
+DELIMITER //
+
+CREATE TRIGGER verificarlicenciaconducir_id
+AFTER INSERT ON verificarlicenciaconducir
+FOR EACH ROW
+BEGIN
+    INSERT INTO registros (numtabla)
+    VALUES (FIDtabla('verificarlicenciaconducir'));
+END//
+DELIMITER;
+
+
 DELIMITER //
 CREATE FUNCTION FIDtabla(nombre_tabla VARCHAR(50)) 
 RETURNS INT
@@ -44,154 +205,3 @@ BEGIN
     RETURN aux;
 END//
 DELIMITER ;
-
-DELIMITER//
-
--- Trigger para que funcione con todas las tablas
-CREATE TRIGGER certificadoMatrimonio_id
-AFTER INSERT ON certificadoMatrimonio
-FOR EACH ROW
-BEGIN
-    INSERT INTO registros (numtabla)
-    VALUES (FIDtabla('certificadoMatrimonio'));
-END//
-
-DELIMITER//
-
-CREATE TRIGGER apostilla_id
-AFTER INSERT ON apostilla
-FOR EACH ROW
-BEGIN
-    INSERT INTO registros (numtabla)
-    VALUES (FIDtabla('apostilla'));
-END//
-
-DELIMITER//
-
-CREATE TRIGGER certificadocontinuidadmatrimonio_id
-AFTER INSERT ON certificadocontinuidadmatrimonio
-FOR EACH ROW
-BEGIN
-    INSERT INTO registros (numtabla)
-    VALUES (FIDtabla('certificadocontinuidadmatrimonio'));
-END//
-
-DELIMITER//
-
-CREATE TRIGGER certificadoinscripcionconsular_id
-AFTER INSERT ON certificadoinscripcionconsular
-FOR EACH ROW
-BEGIN
-    INSERT INTO registros (numtabla)
-    VALUES (FIDtabla('certificadoinscripcionconsular')); -- llamo a la función para la única variable de la tabla registros que no se rellena sola 
-END//
-
-DELIMITER//
-
-CREATE TRIGGER certificadonacionalidad_id
-AFTER INSERT ON certificadonacionalidad
-FOR EACH ROW
-BEGIN
-    INSERT INTO registros (numtabla)
-    VALUES (FIDtabla('certificadonacionalidad'));
-END//
-
-DELIMITER//
-
-CREATE TRIGGER datoshacerunpoder_id
-AFTER INSERT ON datoshacerunpoder
-FOR EACH ROW
-BEGIN
-    INSERT INTO registros (numtabla)
-    VALUES (FIDtabla('datoshacerunpoder'));
-END//
-
-DELIMITER//
-
-CREATE TRIGGER declaracionesjuradas_id
-AFTER INSERT ON declaracionesjuradas
-FOR EACH ROW
-BEGIN
-    INSERT INTO registros (numtabla)
-    VALUES (FIDtabla('declaracionesjuradas'));
-END//
-
-DELIMITER//
-
-CREATE TRIGGER documentoviaje_id
-AFTER INSERT ON documentoviaje
-FOR EACH ROW
-BEGIN
-    INSERT INTO registros (numtabla)
-    VALUES (FIDtabla('documentoviaje'));
-END//
-
-DELIMITER//
-
-CREATE TRIGGER fedevida_id
-AFTER INSERT ON fedevida
-FOR EACH ROW
-BEGIN
-    INSERT INTO registros (numtabla)
-    VALUES (FIDtabla('fedevida'));
-END//
-
-DELIMITER//
-
-CREATE TRIGGER legalizaciones_id
-AFTER INSERT ON legalizaciones
-FOR EACH ROW
-BEGIN
-    INSERT INTO registros (numtabla)
-    VALUES (FIDtabla('legalizaciones'));
-END//
-
-DELIMITER//
-
-CREATE TRIGGER reconocimientoslegales_id
-AFTER INSERT ON reconocimientoslegales
-FOR EACH ROW
-BEGIN
-    INSERT INTO registros (numtabla)
-    VALUES (FIDtabla('reconocimientoslegales'));
-END//
-
-DELIMITER//
-
-CREATE TRIGGER registrodefuncion_id
-AFTER INSERT ON registrodefuncion
-FOR EACH ROW
-BEGIN
-    INSERT INTO registros (numtabla)
-    VALUES (FIDtabla('registrodefuncion'));
-END//
-
-DELIMITER//
-
-CREATE TRIGGER registromatrimonio_id
-AFTER INSERT ON registromatrimonio
-FOR EACH ROW
-BEGIN
-    INSERT INTO registros (numtabla)
-    VALUES (FIDtabla('registromatrimonio'));
-END//
-
-DELIMITER//
-
-CREATE TRIGGER registronacimiento_id
-AFTER INSERT ON registronacimiento
-FOR EACH ROW
-BEGIN
-    INSERT INTO registros (numtabla)
-    VALUES (FIDtabla('registronacimiento'));
-END//
-
-DELIMITER//
-
-CREATE TRIGGER verificarlicenciaconducir_id
-AFTER INSERT ON verificarlicenciaconducir
-FOR EACH ROW
-BEGIN
-    INSERT INTO registros (numtabla)
-    VALUES (FIDtabla('verificarlicenciaconducir'));
-END//
